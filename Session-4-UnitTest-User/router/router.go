04e2d9eda62714335670c1ace4ae5c446f0fc8cb
fahmi arsyad <fahmi.arsyad@praisindo.co.id>
2024-06-13 11:32:57 +0700

    session-4-unittest-user

---
+++ Session-4-UnitTest-User/router/router.go
@@ -0,0 +1,42 @@
+package router
+
+import (
+	"Training/Session-4-UnitTest-User/handler"
+	"Training/Session-4-UnitTest-User/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+// func SetupRouter(r *gin.Engine) {
+// 	usersPublicEndpoint := r.Group("/users")
+// 	usersPublicEndpoint.GET("/:id", handler.GetUser)
+// 	usersPublicEndpoint.GET("/", handler.GetAllUsers)
+// 	usersPublicEndpoint.GET("/ALL/:name", handler.GetUserByName)
+// 	usersPrivateEndpoint := r.Group("/users")
+// 	usersPrivateEndpoint.Use(middleware.AuthMiddleware())
+// 	usersPrivateEndpoint.POST("/", handler.CreateUser)
+// 	usersPrivateEndpoint.PUT("/:id", handler.UpdateUser)
+// 	usersPrivateEndpoint.DELETE("/:id", handler.DeleteUser)
+// }
+
+func SetupRouter(r *gin.Engine, userHandler handler.IUserHandler) {
+	// Mengatur endpoint publik untuk pengguna
+	usersPublicEndpoint := r.Group("/users")
+	// Rute untuk mendapatkan pengguna berdasarkan ID
+	usersPublicEndpoint.GET("/:id", userHandler.GetUser)
+	// Rute untuk mendapatkan semua pengguna
+	usersPublicEndpoint.GET("", userHandler.GetAllUsers)
+	usersPublicEndpoint.GET("/", userHandler.GetAllUsers)
+
+	// Mengatur endpoint privat untuk pengguna dengan middleware autentikasi
+	usersPrivateEndpoint := r.Group("/users")
+	// Menambahkan middleware autentikasi untuk endpoint privat
+	usersPrivateEndpoint.Use(middleware.AuthMiddleware())
+	// Rute untuk membuat pengguna baru
+	usersPrivateEndpoint.POST("", userHandler.CreateUser)
+	usersPrivateEndpoint.POST("/", userHandler.CreateUser)
+	// Rute untuk memperbarui pengguna berdasarkan ID
+	usersPrivateEndpoint.PUT("/:id", userHandler.UpdateUser)
+	// Rute untuk menghapus pengguna berdasarkan ID
+	usersPrivateEndpoint.DELETE("/:id", userHandler.DeleteUser)
+}
