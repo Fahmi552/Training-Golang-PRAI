4cfed1096fd2d9b8381630da2940794b453009f1
fahmi arsyad <fahmi.arsyad@praisindo.co.id>
2024-06-14 13:28:21 +0700

    Session-5-Validator

---
+++ Session-5-Validator/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	// "log"
+
+	// "Training/session-2-latihan-crud-user-gin/router"
+	// "Training/Session-4-UnitTest-User/entity"
+	// "Training/Session-4-UnitTest-User/repository/slice"
+	// "Training/Session-4-UnitTest-User/service"
+	// "Training/Session-4-UnitTest-User/handler"
+	// "Training/Session-4-UnitTest-User/router"
+	// "github.com/gin-gonic/gin"
+	// "log"
+
+	"Training/Session-5-Validator/entity"
+	"Training/Session-5-Validator/handler"
+	"Training/Session-5-Validator/repository/slice"
+	"Training/Session-5-Validator/router"
+	"Training/Session-5-Validator/service"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	// setup service
+	var mockUserDBInSlice []entity.User
+	userRepo := slice.NewUserRepository(mockUserDBInSlice)
+	userService := service.NewUserService(userRepo)
+	userHandler := handler.NewUserHandler(userService)
+
+	// Routes
+	router.SetupRouter(r, userHandler)
+
+	// Run the server
+	log.Println("Running server on port 8080")
+	r.Run(":8080")
+}
